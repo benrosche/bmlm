198fa1b4b7c08d124bda543ccb13937b08ed4f6d
Ben Rosche <fr2488@ad.nyu.edu>
2025-10-24 15:32:00 +0400

    updated monetPlot file

--- R/monetPlot.R
+++ R/monetPlot.R
@@ -20,139 +20,102 @@
 #' @export monetPlot
 #' @author Benjamin Rosche <benjamin.rosche@@gmail.com>
 
-monetPlot <- function(rmm, parameter, centrality="median", lab=F, r=3, sav=F) {
+monetPlot <- function(rmm, parameter, label=NULL, r=2, yaxis=T) {
   
   library(ggplot2)
   library(ggmcmc)
   library(coda)
   library(patchwork)
-  library(bayestestR)
+  
+  escape_regex <- function(x) gsub("([][{}()+*^$|.?\\\\])", "\\\\\\1", x)
+  
+  # Checks --------------------------------------------------------------------------------------- #
+  
+  if(is.null(rmm$jags.out)) stop("JAGS output could not be retrieved. Please specify monitor = T when running rmm.")
+  if(is.null(label)) label = parameter
   
   # Get mcmclist and posterior stats ------------------------------------------------------------- #
   
-  if(is.null(rmm$jags.out)) stop("JAGS output could not be retrieved. monitor=T must be specified when running rmm.")
+  mcmc.list <- coda::as.mcmc(rmm$jags.out)
   
-  mcmclist <- 
-    rmm$jags.out %>% 
-    as.mcmc() %>% 
-    .[, parameter, drop = FALSE]
+  mcmc.ggs <- 
+    ggmcmc::ggs(
+      mcmc.list,
+      family = paste0("^", escape_regex(parameter), "$"), 
+      par_labels = data.frame(Parameter = parameter, Label = label)
+    )
   
-  param <- describe_posterior(mcmclist, centrality=centrality, ci=1) 
-  
-  # Modify theme --------------------------------------------------------------------------------- #
-  
-  theme_light2 <- 
-    theme_light() +
-    theme(
-      text = element_text(size = 16),
-      legend.position="none",
-      plot.title = element_blank(),
-      panel.grid.minor = element_blank(),
-      panel.grid.major = element_blank(),
-      strip.text = element_blank()
-    ) 
-  theme_set(theme_light2)
+  p.quantiles <- mcmc.ggs %>% pull(value) %>% quantile(., c(.05, .5, .95)) %>% round(., r)
+  p.mad <- mcmc.ggs %>% pull(value) %>% mad() %>% round(., r)
   
   # Create plots --------------------------------------------------------------------------------- #
   
-  mcmc_ggs <- ggs(mcmclist)
-  
-  # Density plot
-  p1 <- 
-    ggs_density(mcmc_ggs) + 
-    geom_vline(xintercept=0) +
-    geom_vline(xintercept=round(param[,2], r), linetype="dashed") +
-    theme_light2 +
-    theme(
-      axis.ticks=element_blank(), 
-      axis.text.x=element_blank(), 
-      axis.text.y=element_blank()
-    ) +
-    labs(x=NULL, y = "Density", subtitle = paste0("Parameter: ", parameter, ifelse(!isFALSE(lab), paste0(" - ", lab), "")))
-  
-  # Traceplot
-  p2 <- 
-    ggs_traceplot(mcmc_ggs, original_burnin = F) + 
-    geom_hline(yintercept=0) +
-    geom_hline(yintercept=round(param[,2], r), linetype="dashed") +
-    coord_flip() +
-    scale_y_continuous(breaks = c(0, round(param[,2], r), round(param[,4], r), round(param[,5], r)) %>% sort()) +
-    theme_light2 +
-    labs(x = paste0("Iteration (", max(mcmc_ggs$Chain), " chains)"), y = NULL) 
-  
-  p <- 
-    (p1 / p2) +
-    plot_layout(heights = c(1, 1)) +  
-    plot_annotation(theme = theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))) 
-  
-  
-  # Save?
-  if(sav==T) {
-    ggsave(
-      paste0(gsub("[[:space:]]", "", str_squish(parameter)), ".png"),
-      p,
-      width = 11,
-      height = 6, 
-      units='in', 
-      dpi=300
-    )
+  if(isTRUE(yaxis)) {
+    yaxis <-  "Density"
+    xaxis <- paste0("Scans (", mcmc.ggs %>% pull(Chain) %>% max(), " chains)") 
+  } else {
+    yaxis <- ""
+    xaxis <- ""
   }
   
-  return(p)
+  # Density plot
+  p1 <-
+    ggs_density(mcmc.ggs, hpd = TRUE) +
+    geom_vline(xintercept = 0) +
+    geom_vline(xintercept = p.quantiles[2], linetype = "dashed") +
+    labs(x="", y=yaxis, title = paste0("Parameter: ", label)) +
+    theme_minimal() +
+    theme(
+      plot.margin = unit(c(0, 0, 0, 0), "cm"),
+      legend.position = "none",
+      panel.grid = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.text = element_blank(),
+      strip.text = element_blank(),
+      strip.background = element_blank(),
+      plot.title   = element_text(face = "bold", hjust = 0.5, size = 14),
+      axis.title.y = element_text(face = "bold", size = 14)
+    )
+  
+  # Traceplot
+  p2 <-
+    ggs_traceplot(mcmc.ggs, original_burnin = FALSE) +
+    geom_hline(yintercept = 0) +
+    geom_hline(yintercept = p.quantiles[2], linetype = "dashed") +
+    coord_flip() +
+    scale_y_continuous(
+      breaks = c(p.quantiles, 0) %>% as.numeric() %>% sort(),
+      labels = function(x) {
+        labs <- c(
+          setNames(
+            paste0(as.character(p.quantiles), "\n(", names(p.quantiles), ")"),
+            as.character(p.quantiles)
+          ),
+          "0" = "0"
+        )
+        labs[as.character(x)]
+      }
+    ) +
+    labs(
+      x = xaxis,
+      y = ""
+    ) +
+    theme_minimal() +
+    theme(
+      plot.margin = unit(c(-0.6, 0, 0, 0), "cm"),
+      legend.position = "none",
+      panel.grid = element_blank(),
+      axis.text.y = element_blank(),
+      strip.text = element_blank(),
+      axis.title.y = element_text(face = "bold", size = 14),
+      axis.text.x  = element_text(color = "black", size = 12)  
+    )
+  
+  return(
+    (p1 / p2) +
+      plot_layout(heights = c(1, 1)) +  
+      plot_annotation(theme = theme(plot.margin = unit(c(0, 0, 0, 0), "cm"))) 
+  )
+  
 }
-
-# NEW VERSION (INTEGRATE!)
-# monetPlot <- function(g.ergm, param, paramlabel = NULL, breaks = waiver(), yaxis = T) {
-#   
-#   if (is.null(paramlabel)) paramlabel = param
-#   
-#   if (is.null(g.ergm$sample)) stop("Not estimated by MCMC ...")
-#   mcmclist <- g.ergm$sample %>% as.mcmc.list()
-#   
-#   ggs.obj <- ggs(mcmclist, family = paste0("A", escapeRegex(param), "$"), 
-#                  par_labels = data.frame(Parameter = param, Label = paramlabel))
-#   
-#   p.median <- ggs.obj %>% pull(value) %>% median()
-#   
-#   p1 <-
-#     ggs_density(ggs.obj, hpd = TRUE) +
-#     geom_vline(xintercept = 0) +
-#     geom_vline(xintercept = p.median, linetype = "dashed") +
-#     scale_color_viridis_d(direction = -1) +
-#     scale_fill_viridis_d(direction = -1) +
-#     labs(y = if (isTRUE(yaxis)) "Density" else "") +
-#     theme_minimal() +
-#     theme(
-#       plot.margin = unit(c(0, 0, 0, 0), "cm"),
-#       legend.position = "none",
-#       panel.grid = element_blank(),
-#       axis.ticks = element_blank(),
-#       axis.title.x = element_blank(),
-#       axis.text = element_blank(),
-#       strip.text = element_text(size=15, face-"bold", color="black"),
-#       strip.background = element_blank()
-#     )
-#   
-#   p2 <-
-#     ggs_traceplot(ggs.obj, original_burnin = FALSE) +
-#     geom_hline(yintercept = 0) +
-#     geom_hline(yintercept = p.median, linetype = "dashed") +
-#     coord_flip() +
-#     scale_y_continuous(breaks = breaks) +
-#     scale_color_viridis_d(direction = -1) +
-#     labs(
-#       x = if (isTRUE(yaxis)) paste0("Scans (", ggs.obj %>% pull(Chain) %>% max(), " chains)") else "",
-#       y = ""
-#     ) +
-#     theme_minimal() +
-#     theme(
-#       plot.margin = unit(c(-0.6, 0, 0, 0), "cm"),
-#       legend.position = "none",
-#       panel.grid = element_blank(),
-#       axis.text.y = element_blank(),
-#       strip.text = element_blank()
-#     )
-#   
-#   return(plot_grid(p1, p2, align = "v", nrow = 2))
-# }
-
